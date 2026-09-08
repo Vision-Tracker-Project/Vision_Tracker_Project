3a1996eb1bf2521f8beebac02f9400b89a45d5bf
leesee1393 <leesee1393@gmail.com>
2026-09-08 15:36:07 +0900

    feat: update STM32 firmware and servo UART control

--- STM32/exception.c
+++ STM32/exception.c
@@ -1,5 +1,8 @@
 #include "device_driver.h"
 
+volatile uint8_t g_rx_packet[PACKET_SIZE];
+volatile uint8_t g_packet_ready = 0; // 메인 루프로 보낼 완료 신호
+static uint8_t s_rx_index = 0;
 
 void _Invalid_ISR(void)
 {
@@ -11,8 +14,31 @@ void _Invalid_ISR(void)
 
 void USART2_IRQHandler(void)
 {
-    if (Macro_Check_Bit_Set(USART2->SR, 5))
+    if (USART2->SR & (1 << 5)) // RXNE 확인
     {
-        Uart2_RX_Push_From_ISR((unsigned char)(USART2->DR & 0xFFU));
+        uint8_t byte = (uint8_t)(USART2->DR & 0xFF);
+
+        // 1. 헤더 동기화 및 바이트 적재
+        if (s_rx_index == 0)
+        {
+            if (byte == PACKET_HEADER) g_rx_packet[s_rx_index++] = byte;
+        }
+        else if (s_rx_index < PACKET_SIZE)
+        {
+            g_rx_packet[s_rx_index++] = byte;
+
+            // 2. 6바이트가 모두 찼을 때 검증
+            if (s_rx_index == PACKET_SIZE)
+            {
+                uint8_t checksum = (g_rx_packet[1] + g_rx_packet[2] + g_rx_packet[3]) & 0xFF;
+                
+                // 테일과 체크섬이 모두 맞으면 깃발 세우기
+                if ((g_rx_packet[5] == PACKET_TAIL) && (g_rx_packet[4] == checksum))
+                {
+                    g_packet_ready = 1; 
+                }
+                s_rx_index = 0; // 다음 수신 준비
+            }
+        }
     }
 }
--- STM32/uart.c
+++ STM32/uart.c
@@ -1,9 +1,5 @@
 #include "device_driver.h"
 
-#define PACKET_SIZE 6U
-#define PACKET_HEADER 0xAAU
-#define PACKET_TAIL 0x55U
-
 #define TARGET_PAN 0x01U
 #define TARGET_TILT 0x02U
 #define ACTION_SET_ANGLE 0x01U
@@ -11,180 +7,59 @@
 
 #define UART_RX_BUFFER_SIZE 64U
 
-static unsigned char packet_buffer[PACKET_SIZE];
-static unsigned char packet_index = 0U;
-
-static volatile unsigned char uart_rx_buffer[UART_RX_BUFFER_SIZE];
-static volatile unsigned int uart_rx_head = 0U;
-static volatile unsigned int uart_rx_tail = 0U;
-
 void Uart2_Init(int baud)
 {
-    double div;
-    unsigned int mant;
-    unsigned int frac;
-    volatile unsigned int t;
+  double div;
+  unsigned int mant;
+  unsigned int frac;
 
-    Macro_Set_Bit(RCC->AHB1ENR, 0);
-    Macro_Set_Bit(RCC->APB1ENR, 17);
-    Macro_Write_Block(GPIOA->MODER, 0xf, 0xa, 4);
-    Macro_Write_Block(GPIOA->AFR[0], 0xff, 0x77, 8);
-    Macro_Write_Block(GPIOA->PUPDR, 0xf, 0x5, 4);
+  Macro_Set_Bit(RCC->AHB1ENR, 0);                   // PA2,3
+  Macro_Set_Bit(RCC->APB1ENR, 17);                   // USART2 ON
+  Macro_Write_Block(GPIOA->MODER, 0xf, 0xa, 4);     // PA2,3 => ALT
+  Macro_Write_Block(GPIOA->AFR[0], 0xff, 0x77, 8);  // PA2,3 => AF07
+  Macro_Write_Block(GPIOA->PUPDR, 0xf, 0x5, 4);     // PA2,3 => Pull-Up  
 
-    t = GPIOA->LCKR & 0x7FFF;
-    GPIOA->LCKR = (0x1U << 16) | t | (0x3U << 2);
-    GPIOA->LCKR = (0x0U << 16) | t | (0x3U << 2);
-    GPIOA->LCKR = (0x1U << 16) | t | (0x3U << 2);
-    t = GPIOA->LCKR;
-    (void)t;
+  volatile unsigned int t = GPIOA->LCKR & 0x7FFF;
+  GPIOA->LCKR = (0x1<<16)|t|(0x3<<2);                // Lock PA2, 3 Configuration
+  GPIOA->LCKR = (0x0<<16)|t|(0x3<<2);
+  GPIOA->LCKR = (0x1<<16)|t|(0x3<<2);
+  t = GPIOA->LCKR;
 
-    div = PCLK1 / (16.0 * baud);
-    mant = (unsigned int)div;
-    frac = (unsigned int)((div - mant) * 16.0 + 0.5);
-    mant += frac >> 4;
-    frac &= 0xfU;
+  div = PCLK1/(16. * baud);
+  mant = (int)div;
+  frac = (int)((div - mant) * 16. + 0.5);
+  mant += frac >> 4;
+  frac &= 0xf;
 
-    USART2->BRR = (mant << 4) | frac;
-    USART2->CR1 = (1U << 13) | (1U << 3) | (1U << 2);
-    USART2->CR2 = 0U;
-    USART2->CR3 = 0U;
+  USART2->BRR = (mant<<4)|(frac<<0);
+  USART2->CR1 = (1<<13)|(0<<12)|(0<<10)|(1<<3)|(1<<2);
+  USART2->CR2 = 0<<12;
+  USART2->CR3 = 0;
 }
 
 void Uart2_Send_Byte(char data)
 {
-    if (data == '\n')
-    {
-        while (!Macro_Check_Bit_Set(USART2->SR, 7));
-        USART2->DR = 0x0d;
-    }
+  if(data == '\n')
+  {
+    while(!Macro_Check_Bit_Set(USART2->SR, 7));
+    USART2->DR = 0x0d;
+  }
 
-    while (!Macro_Check_Bit_Set(USART2->SR, 7));
-    USART2->DR = (unsigned char)data;
-}
-
-void Uart2_Send_String(char *pt)
-{
-    while (*pt != 0)
-    {
-        Uart2_Send_Byte(*pt++);
-    }
-}
-
-char Uart2_Get_Pressed(void)
-{
-    if (USART2->SR & (1U << 5))
-    {
-        return (char)(USART2->DR & 0xFFU);
-    }
-
-    return 0;
+  while(!Macro_Check_Bit_Set(USART2->SR, 7));
+  USART2->DR = data;
 }
 
 void Uart2_RX_Interrupt_Enable(int en)
 {
-    if (en)
-    {
-        Macro_Set_Bit(USART2->CR1, 5);
-        NVIC_ClearPendingIRQ(USART2_IRQn);
-        NVIC_EnableIRQ(USART2_IRQn);
-    }
-    else
-    {
-        Macro_Clear_Bit(USART2->CR1, 5);
-        NVIC_DisableIRQ(USART2_IRQn);
-    }
-}
-
-void Uart2_RX_Push_From_ISR(unsigned char data)
-{
-    unsigned int next_head = (uart_rx_head + 1U) % UART_RX_BUFFER_SIZE;
-
-    if (next_head != uart_rx_tail)
-    {
-        uart_rx_buffer[uart_rx_head] = data;
-        uart_rx_head = next_head;
-    }
-}
-
-static int Uart2_Read_Byte(unsigned char *data)
-{
-    if (uart_rx_head == uart_rx_tail)
-    {
-        return 0;
-    }
-
-    *data = uart_rx_buffer[uart_rx_tail];
-    uart_rx_tail = (uart_rx_tail + 1U) % UART_RX_BUFFER_SIZE;
-    return 1;
-}
-
-static void Uart2_Send_Raw(const unsigned char *data, unsigned int length)
-{
-    unsigned int i;
-
-    for (i = 0U; i < length; i++)
-    {
-        while (!Macro_Check_Bit_Set(USART2->SR, 7));
-        USART2->DR = data[i];
-    }
-}
-
-static int Packet_Process(const unsigned char *packet)
-{
-    unsigned char target = packet[1];
-    unsigned char action = packet[2];
-    unsigned char angle = packet[3];
-    unsigned char checksum = packet[4];
-
-    if (packet[0] != PACKET_HEADER || packet[5] != PACKET_TAIL)
-    {
-        return 0;
-    }
-
-    if (checksum != (unsigned char)(target + action + angle))
-    {
-        return 0;
-    }
-
-    if (action != ACTION_SET_ANGLE || angle > SERVO_MAX_ANGLE)
-    {
-        return 0;
-    }
-
-    if (target == TARGET_PAN)
-    {
-        TIM2_Servo_Set_Pan_Angle(angle);
-    }
-    else if (target == TARGET_TILT)
-    {
-        TIM2_Servo_Set_Tilt_Angle(angle);
-    }
-    else
-    {
-        return 0;
-    }
-
-    Uart2_Send_Raw(packet, PACKET_SIZE);
-    return 1;
-}
-
-void Packet_Receive(void)
-{
-    unsigned char received_data;
-
-    while (Uart2_Read_Byte(&received_data))
-    {
-        if (packet_index == 0U && received_data != PACKET_HEADER)
-        {
-            continue;
-        }
-
-        packet_buffer[packet_index++] = received_data;
-
-        if (packet_index == PACKET_SIZE)
-        {
-            Packet_Process(packet_buffer);
-            packet_index = 0U;
-        }
-    }
+  if(en)
+  {
+    Macro_Set_Bit(USART2->CR1, 5);
+    NVIC_ClearPendingIRQ(38);
+    NVIC_EnableIRQ(38);
+  }
+  else
+  {
+    Macro_Clear_Bit(USART2->CR1, 5);
+    NVIC_DisableIRQ(38);
+  }
 }
